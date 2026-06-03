dace54781703e21b4c025f573aef18fd5ddc24d7
devtobi <28538704+devtobi@users.noreply.github.com>
2026-06-03 16:45:20 +0200

    :recycle: Added BaseView component

---
+++ frontend/src/components/common/BaseView.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container class="fill-height py-14 px-8">
+    <v-row class="justify-space-between align-center">
+      <v-col
+        cols="auto"
+      >
+        <h1 v-if="title" class="ma-0 text-headline-large">
+          {{ title }}
+        </h1>
+      </v-col>
+      <v-col
+        cols="auto"
+      >
+        <slot name="actions" />
+      </v-col>
+    </v-row>
+    <v-divider class="my-4" />
+    <v-row>
+      <slot />
+    </v-row>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title?: string;
+}>();
+
+defineSlots<{
+  default(): void;
+  actions(): void;
+}>();
+</script>
