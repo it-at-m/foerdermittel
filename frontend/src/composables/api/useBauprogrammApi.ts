8871ee704fa7eb53bda850a42429981840f5bd3d
devtobi <28538704+devtobi@users.noreply.github.com>
2026-06-15 17:14:45 +0200

    :alien: Added API-related frontend code for Bauprogramm

---
+++ frontend/src/composables/api/useBauprogrammApi.ts
@@ -0,0 +1,55 @@
+import type {
+  BauprogrammResponseDTO,
+  DeleteBauprogrammRequest,
+  GetBauprogrammeByPageAndSizeRequest,
+  GetBauprogrammRequest,
+  PagedModelBauprogrammResponseDTO,
+  SaveBauprogrammRequest,
+  UpdateBauprogrammRequest,
+} from "@/api/generated/foerdermittel-backend";
+
+import { ApiFactory } from "@/api/ApiFactory";
+import { BauprogrammControllerApi } from "@/api/generated/foerdermittel-backend";
+import useAPI from "@/composables/useAPI";
+
+export function useCreateBauprogramm() {
+  const api = ApiFactory.getInstance(BauprogrammControllerApi);
+
+  return useAPI<SaveBauprogrammRequest, BauprogrammResponseDTO>((params) =>
+    api.saveBauprogramm(params)
+  );
+}
+
+export function useUpdateBauprogramm() {
+  const api = ApiFactory.getInstance(BauprogrammControllerApi);
+
+  return useAPI<UpdateBauprogrammRequest, BauprogrammResponseDTO>((params) =>
+    api.updateBauprogramm(params)
+  );
+}
+
+export function useGetBauprogramme() {
+  const api = ApiFactory.getInstance(BauprogrammControllerApi);
+
+  return useAPI<
+    GetBauprogrammeByPageAndSizeRequest,
+    PagedModelBauprogrammResponseDTO
+  >(() => api.getBauprogrammeByPageAndSize());
+}
+
+export function useGetBauprogramm() {
+  const api = ApiFactory.getInstance(BauprogrammControllerApi);
+
+  return useAPI<GetBauprogrammRequest, BauprogrammResponseDTO>((params) =>
+    api.getBauprogramm(params)
+  );
+}
+
+export function useDeleteBauprogramm() {
+  const api = ApiFactory.getInstance(BauprogrammControllerApi);
+
+  // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
+  return useAPI<DeleteBauprogrammRequest, void>((params) =>
+    api.deleteBauprogramm(params)
+  );
+}
