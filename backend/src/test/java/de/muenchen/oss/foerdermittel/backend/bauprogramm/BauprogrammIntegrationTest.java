006dab72db7213008c27d904d2d7b2e189ad5008
devtobi <28538704+devtobi@users.noreply.github.com>
2026-06-18 17:13:16 +0200

    :construction: Integration tests

---
+++ backend/src/test/java/de/muenchen/oss/foerdermittel/backend/bauprogramm/BauprogrammIntegrationTest.java
@@ -0,0 +1,233 @@
+package de.muenchen.oss.foerdermittel.backend.bauprogramm;
+
+import static de.muenchen.oss.foerdermittel.backend.TestConstants.SPRING_TEST_PROFILE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import de.muenchen.oss.foerdermittel.backend.TestConstants;
+import de.muenchen.oss.foerdermittel.backend.TestSecurityConfiguration;
+import de.muenchen.oss.foerdermittel.backend.bauprogramm.dto.BauprogrammCreateDTO;
+import de.muenchen.oss.foerdermittel.backend.bauprogramm.dto.BauprogrammUpdateDTO;
+import de.muenchen.oss.foerdermittel.backend.bauprogramm.dto.BauprogrammResponseDTO;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.client.RestTestClient;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureRestTestClient
+@ActiveProfiles(profiles = { SPRING_TEST_PROFILE })
+@Import(TestSecurityConfiguration.class)
+class BauprogrammIntegrationTest {
+
+    @Autowired
+    private RestTestClient restTestClient;
+
+    @Container
+    @ServiceConnection
+    @SuppressWarnings("unused")
+    private static final PostgreSQLContainer POSTGRE_SQL_CONTAINER = new PostgreSQLContainer(
+            DockerImageName.parse(TestConstants.TESTCONTAINERS_POSTGRES_IMAGE));
+
+    private static final String ID = "1";
+
+    @Autowired
+    private BauprogrammRepository bauprogrammRepository;
+
+    @BeforeEach
+    public void setUp() {
+        bauprogrammRepository.deleteAll();
+        final Bauprogramm exampleEntity = new Bauprogramm(new BigDecimal(ID), "Test");
+        bauprogrammRepository.save(exampleEntity);
+    }
+
+    @Nested
+    class GetBauprogramm {
+
+        @Test
+        void givenEntityExists_thenReturnEntity() {
+            restTestClient
+                    .get()
+                    .uri("/bauprogramme/{id}", ID)
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer sachbearbeitung")
+                    .exchange()
+                    .expectStatus().isOk()
+                    .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                    .expectBody(BauprogrammResponseDTO.class)
+                    .value(responseDTO -> {
+                        assertNotNull(responseDTO);
+                        assertThat(responseDTO.id()).isEqualTo(ID);
+                    });
+        }
+
+        @Test
+        void givenEntityNotExists_thenReturnNotFound() {
+            restTestClient
+                    .get()
+                    .uri("/bauprogramme/{id}", 2)
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer sachbearbeitung")
+                    .exchange()
+                    .expectStatus().isNotFound();
+        }
+
+    }
+
+    @Nested
+    class GetBauprogramme {
+
+        @Test
+        void givenValidPageNumber_thenReturnPageOfEntities() {
+            restTestClient.get()
+                    .uri(uriBuilder -> uriBuilder
+                            .path("/bauprogramme")
+                            .queryParam("pageNumber", "0")
+                            .build())
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer sachbearbeitung")
+                    .exchange()
+                    .expectStatus().isOk()
+                    .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                    .expectBody()
+                    .jsonPath("$.content")
+                    .value(new ParameterizedTypeReference<List<BauprogrammResponseDTO>>() {
+                    }, content -> assertThat(content.size()).isEqualTo(1));
+        }
+
+        @Test
+        void givenInvalidPageNumber_thenReturnEmptyPage() {
+            restTestClient.get()
+                    .uri(uriBuilder -> uriBuilder
+                            .path("/bauprogramme")
+                            .queryParam("pageNumber", "99")
+                            .build())
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer sachbearbeitung")
+                    .exchange()
+                    .expectStatus().isOk()
+                    .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                    .expectBody()
+                    .jsonPath("$.content")
+                    .value(new ParameterizedTypeReference<List<BauprogrammResponseDTO>>() {
+                    }, content -> assertThat(content).isEmpty());
+        }
+
+    }
+
+    @Nested
+    class SaveEntity {
+
+        @Test
+        void givenEntityNotExists_thenEntityIsSaved() {
+            final BauprogrammCreateDTO requestDTO = new BauprogrammCreateDTO(5, "Test");
+
+            final BauprogrammResponseDTO responseDTO = restTestClient.post()
+                    .uri("/bauprogramme")
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer admin")
+                    .body(requestDTO)
+                    .accept(MediaType.APPLICATION_JSON)
+                    .exchange()
+                    .expectStatus().isCreated()
+                    .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                    .expectBody(BauprogrammResponseDTO.class)
+                    .value(response -> {
+                        assertThat(response).isNotNull();
+                        assertThat(response.bauprogramm()).isEqualTo(requestDTO.bauprogramm());
+                        assertThat(response.bezeichnung()).isEqualTo(requestDTO.bezeichnung());
+                    })
+                    .returnResult()
+                    .getResponseBody();
+
+            assertThat(responseDTO).isNotNull();
+            final Optional<Bauprogramm> entity = bauprogrammRepository.findById(Integer.valueOf(responseDTO.id()));
+            assertThat(entity).isPresent();
+            assertThat(entity.get().getBauprogramm().intValue()).isEqualTo(requestDTO.bauprogramm());
+            assertThat(entity.get().getBezeichnung()).isEqualTo(requestDTO.bezeichnung());
+        }
+
+//        @Test
+//        void givenEntityAlreadyExists_thenReturnBadRequest() {
+//            final BauprogrammCreateDTO requestDTO = new BauprogrammCreateDTO(Integer.valueOf(ID), "Test");
+//
+//            restTestClient.post()
+//                    .uri("/bauprogramme")
+//                    .header(HttpHeaders.AUTHORIZATION, "Bearer admin")
+//                    .body(requestDTO)
+//                    .accept(MediaType.APPLICATION_JSON)
+//                    .exchange()
+//                    .expectStatus().isBadRequest();
+//        }
+
+        @Test
+        void givenNotHavingAdminRole_thenReturnForbidden() {
+            final BauprogrammCreateDTO requestDTO = new BauprogrammCreateDTO(Integer.valueOf(ID), "Test");
+
+            restTestClient.post()
+                    .uri("/bauprogramme")
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer sachbearbeitung")
+                    .body(requestDTO)
+                    .accept(MediaType.APPLICATION_JSON)
+                    .exchange()
+                    .expectStatus().isForbidden();
+        }
+    }
+
+
+//
+//    @Nested
+//    class UpdateEntity {
+//        @Test
+//        void givenEntity_thenEntityIsUpdated() {
+//            final String newValue = "Test2";
+//            final TheEntityRequestDTO requestDTO = new TheEntityRequestDTO(newValue);
+//
+//            restTestClient.put()
+//                    .uri("/theEntity/{theEntityId}", testEntityId)
+//                    .header(HttpHeaders.AUTHORIZATION, "Bearer writer")
+//                    .body(requestDTO)
+//                    .accept(MediaType.APPLICATION_JSON)
+//                    .exchange()
+//                    .expectStatus().isOk()
+//                    .expectHeader().contentType(MediaType.APPLICATION_JSON)
+//                    .expectBody(TheEntityResponseDTO.class)
+//                    .value(theEntityResponseDTO -> {
+//                        assertNotNull(theEntityResponseDTO);
+//                        assertThat(theEntityResponseDTO.id()).isEqualTo(testEntityId);
+//                        assertThat(theEntityResponseDTO.textAttribute()).isEqualTo(requestDTO.textAttribute());
+//                    });
+//
+//            assertThat(theEntityRepository.findById(testEntityId).orElseThrow().getTextAttribute()).isEqualTo(newValue);
+//        }
+//    }
+//
+//    @Nested
+//    class DeleteEntity {
+//        @Test
+//        void givenEntityId_thenEntityIsDeleted() {
+//            restTestClient.delete()
+//                    .uri("/theEntity/{theEntityID}", testEntityId)
+//                    .header(HttpHeaders.AUTHORIZATION, "Bearer writer")
+//                    .exchange()
+//                    .expectStatus().isOk();
+//
+//            assertThat(theEntityRepository.findById(testEntityId)).isEmpty();
+//        }
+//    }
+
+}
