6363d95850f6d2927b55f1dcff647ce9aa708550
devtobi <28538704+devtobi@users.noreply.github.com>
2026-06-22 11:36:19 +0200

    :safety_vest: Added rules for min/max checks

--- frontend/src/plugins/rules.ts
+++ frontend/src/plugins/rules.ts
@@ -4,7 +4,14 @@ import vuetify from "@/plugins/vuetify";
 
 export default createRulesPlugin(
   {
-    aliases: {},
+    aliases: {
+      min: (minNumber, err) => {
+        return v => v >= minNumber || err || `Der Wert muss mindestens ${minNumber} betragen.`
+      },
+      max: (maxNumber, err) => {
+        return v => v <= maxNumber || err || `Der Wert darf höchstens ${maxNumber} betragen.`
+      },
+    },
   },
   vuetify.locale
 );
