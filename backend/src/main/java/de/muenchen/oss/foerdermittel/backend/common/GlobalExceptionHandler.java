cd54e622492804b50b7363c2875b791f74ec1730
devtobi <28538704+devtobi@users.noreply.github.com>
2026-06-29 14:48:50 +0200

    :bug: Fixed GlobalExceptionHandler to use ProblemDetail instead of rethrowing

--- backend/src/main/java/de/muenchen/oss/foerdermittel/backend/common/GlobalExceptionHandler.java
+++ backend/src/main/java/de/muenchen/oss/foerdermittel/backend/common/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
 package de.muenchen.oss.foerdermittel.backend.common;
 
-import jakarta.validation.ConstraintViolationException;
 import org.hibernate.PropertyValueException;
+import org.hibernate.exception.ConstraintViolationException;
 import org.hibernate.exception.DataException;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,16 +13,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public void handleDataIntegrityViolationException(final DataIntegrityViolationException ex) {
-        Throwable root = ex.getMostSpecificCause();
+    public ProblemDetail handleDataIntegrityViolationException(final DataIntegrityViolationException ex) {
+        Throwable cause = ex.getCause();
 
-        switch (root) {
-            case ConstraintViolationException cve -> throw new ConflictException(cve.getMessage());
+        return switch (cause) {
+            case ConstraintViolationException cve ->
+                    ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, cve.getMessage());
             case PropertyValueException pve ->
-                    throw new BadRequestException(String.format("Missing required field '%s' for entity '%s'", pve.getPropertyName(), pve.getEntityName()));
-            case DataException de -> throw new BadRequestException(String.format("Invalid field value: %s", de.getMessage()));
-            default -> throw new InternalServerErrorException(ex.getMessage());
-        }
+                    ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, String.format("Missing required field '%s' for entity '%s'", pve.getPropertyName(), pve.getEntityName()));
+            case DataException de ->
+                    ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, String.format("Invalid field value: %s", de.getMessage()));
+            case null, default -> ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+        };
     }
 
 }
