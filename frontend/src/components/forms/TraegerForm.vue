3a2b7a93cf68a41c6e4707676b5d290baf30a58f
xGnouHQ <181658429+xGnouHQ@users.noreply.github.com>
2026-07-01 16:27:17 +0200

    :construction: WIP Traeger frontend route and component

---
+++ frontend/src/components/forms/TraegerForm.vue
@@ -0,0 +1,86 @@
+<template>
+  <v-form
+    ref="form"
+    :readonly="displayMode === InputDisplayMode.READ"
+    @update:model-value="onValidityChanged"
+  >
+    <v-row>
+      <v-col cols="3">
+        <fm-number-input
+          v-model="modelValue.kurzform"
+          :display-mode="displayMode"
+          disable-edit
+          required
+          :rules="[
+            rules.required(),
+            rules.number(),
+            rules['min']!(1),
+            rules['max']!(99),
+            rules['unique']!(traegerFormContext.traeger, currentTraeger),
+          ]"
+          :label="t('model.traeger.kurzform')"
+        />
+      </v-col>
+      <v-col cols="9">
+        <fm-text-field
+          v-model="modelValue.bezeichnung"
+          :display-mode="displayMode"
+          required
+          :counter="200"
+          :rules="[rules.required(), rules.maxLength(200)]"
+          :label="t('model.traeger.bezeichnung')"
+        />
+      </v-col>
+    </v-row>
+  </v-form>
+</template>
+
+<script setup lang="ts">
+import type {
+  TraegerFormContext,
+  TraegerResponseDTO,
+} from "@/api/generated/foerdermittel-backend";
+import type { DeepReadonly } from "vue";
+import type { VForm } from "vuetify/components";
+
+import { ref, useTemplateRef } from "vue";
+import { useI18n } from "vue-i18n";
+import { useRules } from "vuetify/labs/rules";
+
+import FmNumberInput from "@/components/common/FmNumberInput.vue";
+import FmTextField from "@/components/common/FmTextField.vue";
+import { InputDisplayMode } from "@/types/InputDisplayMode";
+
+const { t } = useI18n();
+
+const modelValue = defineModel<Partial<TraegerResponseDTO>>({
+  required: true,
+});
+const currentTraeger = ref(modelValue.value.kurzform);
+
+const { traegerFormContext, displayMode = InputDisplayMode.CREATE } =
+  defineProps<{
+    traegerFormContext: DeepReadonly<TraegerFormContext>;
+    displayMode?: InputDisplayMode;
+  }>();
+
+const emit = defineEmits<{
+  isValid: [boolean | null];
+}>();
+
+function onValidityChanged(newIsValid: boolean | null) {
+  emit("isValid", newIsValid);
+}
+
+const rules = useRules();
+
+const formRef = useTemplateRef<VForm>("form");
+async function validate() {
+  if (formRef.value) {
+    await formRef.value.validate();
+  }
+}
+defineExpose({
+  validate,
+});
+</script>
---
+++ frontend/src/routes/traeger.vue
@@ -0,0 +1,153 @@
+<template>
+  <base-view :domain-key="domainKey">
+    <!-- @vue-generic {Partial<TraegerResponseDTO>} -->
+    <crud-card
+      ref="crudRef"
+      v-model="dataTableOptions"
+      :empty-item-template="EMPTY_ITEM_TEMPLATE"
+      :loading="loading"
+      :table-headers="headers"
+      :domain-key="domainKey"
+      :enable-actions="isAdmin"
+      :items="traeger?.content ?? []"
+      :total-items="traeger?.page?.totalElements ?? 0"
+      :dialog-width="DialogWidth.MEDIUM"
+      @delete="handleDelete"
+      @create="handleCreate"
+      @update="handleUpdate"
+    >
+      <template #form="{ item, updateValidity, inputDisplayMode }">
+        <traeger-form
+          v-if="traegerFormContext"
+          :model-value="item"
+          :display-mode="inputDisplayMode"
+          :traeger-form-context="traegerFormContext"
+          @is-valid="updateValidity"
+        />
+      </template>
+    </crud-card>
+  </base-view>
+</template>
+
+<script setup lang="ts">
+import type { TraegerResponseDTO } from "@/api/generated/foerdermittel-backend";
+import type { DataTableHeader } from "vuetify/framework";
+
+import { computed } from "vue";
+import { useI18n } from "vue-i18n";
+
+import BaseView from "@/components/common/BaseView.vue";
+import CrudCard from "@/components/common/CrudCard.vue";
+import TraegerForm from "@/components/forms/TraegerForm.vue";
+import {
+  useCreateTraeger,
+  useDeleteTraeger,
+  useGetTraeger,
+  useGetTraegerFormContext,
+  useUpdateTraeger,
+} from "@/composables/api/useTraegerApi";
+import useHasAnyRole from "@/composables/useHasAnyRole";
+import usePagination from "@/composables/usePagination";
+import { DialogWidth } from "@/types/DialogWidth";
+import { Role } from "@/types/Role";
+
+const domainKey = "model.traeger.modelName";
+
+const isAdmin = useHasAnyRole(Role.ADMIN);
+
+const { t } = useI18n();
+
+const headers: DataTableHeader<Partial<TraegerResponseDTO>>[] = [
+  {
+    title: t("model.traeger.kurzform"),
+    value: "kurzform",
+    align: "center",
+    width: 100,
+  },
+  { title: t("model.traeger.bezeichnung"), value: "bezeichnung" },
+];
+
+const EMPTY_ITEM_TEMPLATE: Partial<TraegerResponseDTO> = {
+  kurzform: undefined,
+  bezeichnung: "",
+};
+
+const {
+  data: traeger,
+  call: getTraeger,
+  loading: getTraegerLoading,
+} = useGetTraeger();
+
+const {
+  data: traegerFormContext,
+  call: getTraegerFormContext,
+  loading: getTraegerFormContextLoading,
+} = useGetTraegerFormContext();
+
+const { dataTableOptions, onSuccess, onFailure } = usePagination(
+  getTraeger,
+  getTraegerFormContext
+);
+
+const { call: createTraeger, loading: createTraegerLoading } =
+  useCreateTraeger();
+
+const handleCreate = async (traegerCreateDTO: Partial<TraegerResponseDTO>) => {
+  // TODO: some type checking improvements
+  const model = traegerCreateDTO as TraegerResponseDTO;
+  await createTraeger({
+    traegerCreateDTO: model,
+  });
+  if (!createTraegerError.value) {
+    await onSuccess(t("common.message.created", [t(domainKey)]));
+  } else {
+    onFailure(t("common.message.createdError", [t(domainKey)]));
+  }
+};
+
+const {
+  call: updateTraeger,
+  loading: updateTraegerLoading,
+  error: updateTraegerError,
+} = useUpdateTraeger();
+
+const handleUpdate = async (traegerUpdateDTO: Partial<TraegerResponseDTO>) => {
+  // TODO: some type checking improvements
+  const model = traegerUpdateDTO as TraegerResponseDTO;
+  await updateTraeger({
+    id: model.id,
+    traegerUpdateDTO: model,
+  });
+  if (!updateTraegerError.value) {
+    await onSuccess(t("common.message.updated", [t(domainKey)]));
+  } else {
+    onFailure(t("common.message.updatedError", [t(domainKey)]));
+  }
+};
+
+const {
+  call: deleteTraeger,
+  loading: deleteTraegerLoading,
+  error: deleteTraegerError,
+} = useDeleteTraeger();
+
+const handleDelete = async (id: string) => {
+  await deleteTraeger({
+    id,
+  });
+  if (!deleteTraegerError.value) {
+    await onSuccess(t("common.message.deleted", [t(domainKey)]));
+  } else {
+    onFailure(t("common.message.deletedError", [t(domainKey)]));
+  }
+};
+
+const loading = computed(
+  () =>
+    getTraegerLoading.value ||
+    getTraegerFormContextLoading.value ||
+    createTraegerLoading.value ||
+    updateTraegerLoading.value ||
+    deleteTraegerLoading.value
+);
+</script>
