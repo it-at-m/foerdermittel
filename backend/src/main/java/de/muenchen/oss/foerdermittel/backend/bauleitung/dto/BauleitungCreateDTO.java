7502f9e5b37a23b6f3f75bd664252b17e421070d
WadimWeinberger <267319170+WadimWeinberger@users.noreply.github.com>
2026-07-02 15:51:02 +0200

    add test classes

--- backend/src/main/java/de/muenchen/oss/foerdermittel/backend/bauleitung/dto/BauleitungCreateDTO.java
+++ backend/src/main/java/de/muenchen/oss/foerdermittel/backend/bauleitung/dto/BauleitungCreateDTO.java
@@ -5,5 +5,5 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-public record BauleitungCreateDTO(@NotNull @Min(1) @Max(99) Integer bauleitung, @NotNull @Size(min = 1, max = 200) String bezeichnung) {
+public record BauleitungCreateDTO(@NotNull @Min(1) @Max(99) String bauleitung, @NotNull @Size(min = 1, max = 200) String bezeichnung) {
 }
---
+++ backend/src/test/java/de/muenchen/oss/foerdermittel/backend/bauleitung/BauleitungMapperTest.java
@@ -0,0 +1,71 @@
+package de.muenchen.oss.foerdermittel.backend.bauleitung;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import de.muenchen.oss.foerdermittel.backend.bauleitung.dto.BauleitungCreateDTO;
+import de.muenchen.oss.foerdermittel.backend.bauleitung.dto.BauleitungMapper;
+import de.muenchen.oss.foerdermittel.backend.bauleitung.dto.BauleitungResponseDTO;
+import de.muenchen.oss.foerdermittel.backend.bauleitung.dto.BauleitungUpdateDTO;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+
+public class BauleitungMapperTest {
+
+    private final BauleitungMapper bauleitungMapper = Mappers.getMapper(BauleitungMapper.class);
+
+    @Nested
+    class ToDTO {
+
+        @Test
+        void givenEntity_thenReturnsCorrectDTO() {
+            // given
+            Bauleitung entity = new Bauleitung("1", "test");
+
+            // when
+            BauleitungResponseDTO dto = bauleitungMapper.toDTO(entity);
+
+            // then
+            assertThat(dto).isNotNull();
+            assertThat(dto.id()).isEqualTo(entity.getBauleitung().toString());
+            assertThat(dto.bauleitung()).isEqualTo(entity.getBauleitung());
+            assertThat(dto.bezeichnung()).isEqualTo(entity.getBezeichnung());
+        }
+
+    }
+
+    @Nested
+    class ToEntity {
+
+        @Test
+        void givenCreateDTO_thenReturnsCorrectEntity() {
+            // given
+            BauleitungCreateDTO dto = new BauleitungCreateDTO("1", "Test 1");
+
+            // when
+            Bauleitung entity = bauleitungMapper.toEntity(dto);
+
+            // then
+            assertThat(entity).isNotNull();
+            assertThat(entity.getBauleitung()).isEqualTo(dto.bauleitung());
+            assertThat(entity.getBezeichnung()).isEqualTo(dto.bezeichnung());
+        }
+
+        @Test
+        void givenUpdateDTO_thenReturnsCorrectEntity() {
+            // given
+            BauleitungUpdateDTO dto = new BauleitungUpdateDTO("Test 2");
+
+            // when
+            Bauleitung entity = bauleitungMapper.toEntity(dto);
+
+            // then
+            assertThat(entity).isNotNull();
+            assertThat(entity.getBauleitung()).isNull();
+            assertThat(entity.getBezeichnung()).isEqualTo(dto.bezeichnung());
+        }
+
+    }
+
+}
+
