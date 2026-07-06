5b4257f140bdcd23f297e38f22edaa607ebeb188
WadimWeinberger <267319170+WadimWeinberger@users.noreply.github.com>
2026-07-06 08:17:00 +0200

    edit constraints of BauleitungCreateDTO

--- backend/src/main/java/de/muenchen/oss/foerdermittel/backend/bauleitung/dto/BauleitungCreateDTO.java
+++ backend/src/main/java/de/muenchen/oss/foerdermittel/backend/bauleitung/dto/BauleitungCreateDTO.java
@@ -1,9 +1,6 @@
 package de.muenchen.oss.foerdermittel.backend.bauleitung.dto;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
-public record BauleitungCreateDTO(@NotNull @Min(1) @Max(99) String bauleitung, @NotNull @Size(min = 1, max = 200) String bezeichnung) {
+public record BauleitungCreateDTO(@NotNull @Size(min = 1, max = 1) @Pattern(regexp = "^(?:[1-9]|[1-9][0-9])$") String bauleitung, @NotNull @Size(min = 1, max = 200) String bezeichnung) {
 }
