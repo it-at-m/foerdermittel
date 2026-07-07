710da5ff24c653637fc19a859f350bbeed21e235
devtobi <28538704+devtobi@users.noreply.github.com>
2026-07-07 13:25:36 +0200

    :sparkles: Added BenutzerhinweisDialog to BaseView

--- frontend/src/components/common/BaseView.vue
+++ frontend/src/components/common/BaseView.vue
@@ -1,4 +1,11 @@
 <template>
+  <benutzerhinweis-dialog
+    :loading="loading"
+    :benutzerhinweis="benutzerhinweis ?? EMPTY_ITEM_TEMPLATE"
+    :display-mode="displayMode"
+    @close="showBenutzerhinweisDialog = false"
+    @save="handleSave"
+  />
   <v-container
     fluid
     class="d-flex flex-column fill-height pt-10 px-8 overflow-hidden"
@@ -14,6 +21,18 @@
       </v-col>
       <v-col cols="auto">
         <slot name="actions" />
+        <v-tooltip
+          :text="t('common.generics.show', [benutzerhinweiseDomain])"
+          location="left"
+        >
+          <template #activator="{ props }">
+            <v-btn
+              :icon="mdiHelpCircle"
+              v-bind="props"
+              @click="showBenutzerhinweisDialog = true"
+            />
+          </template>
+        </v-tooltip>
       </v-col>
     </v-row>
     <v-divider class="my-4" />
@@ -24,11 +43,28 @@
 </template>
 
 <script setup lang="ts">
-import { computed, watch } from "vue";
+import type {
+  BenutzerhinweisCreateDTO,
+  BenutzerhinweisResponseDTO,
+  BenutzerhinweisUpdateDTO,
+} from "@/api/generated/foerdermittel-backend";
+
+import { mdiHelpCircle } from "@mdi/js";
+import { computed, ref, watch } from "vue";
 import { useI18n } from "vue-i18n";
 import { useRoute } from "vue-router";
 
-import { useGetBenutzerhinweis } from "@/composables/api/useBenutzerhinweisApi";
+import BenutzerhinweisDialog from "@/components/BenutzerhinweisDialog.vue";
+import {
+  useCreateBenutzerhinweis,
+  useGetBenutzerhinweis,
+  useUpdateBenutzerhinweis,
+} from "@/composables/api/useBenutzerhinweisApi";
+import useHasAnyRole from "@/composables/useHasAnyRole";
+import { STATUS_INDICATORS } from "@/constants";
+import { useSnackbarStore } from "@/stores/snackbar";
+import { InputDisplayMode } from "@/types/InputDisplayMode";
+import { Role } from "@/types/Role";
 
 const { domainKey } = defineProps<{
   title?: string;
@@ -43,8 +79,20 @@ const routeName = computed(() => route.name.replace("/", ""));
 const {
   data: benutzerhinweis,
   call: getBenutzerhinweis,
-  loading,
+  loading: getBenutzerhinweisLoading,
 } = useGetBenutzerhinweis();
+
+const {
+  call: createBenutzerhinweis,
+  loading: createBenutzerhinweisLoading,
+  error: createBenutzerhinweisError,
+} = useCreateBenutzerhinweis();
+
+const {
+  call: updateBenutzerhinweis,
+  loading: updateBenutzerhinweisLoading,
+  error: updateBenutzerhinweisError,
+} = useUpdateBenutzerhinweis();
 
 watch(
   routeName,
@@ -57,4 +105,65 @@ watch(
   },
   { immediate: true }
 );
+
+const loading = computed(
+  () =>
+    getBenutzerhinweisLoading.value ||
+    createBenutzerhinweisLoading.value ||
+    updateBenutzerhinweisLoading.value
+);
+
+const isAdmin = useHasAnyRole(Role.ADMIN);
+const showBenutzerhinweisDialog = ref(false);
+const displayMode = computed(() => {
+  if (!showBenutzerhinweisDialog.value) {
+    return undefined;
+  }
+  if (!isAdmin.value) {
+    return InputDisplayMode.READ;
+  }
+  return benutzerhinweis.value
+    ? InputDisplayMode.EDIT
+    : InputDisplayMode.CREATE;
+});
+
+const EMPTY_ITEM_TEMPLATE: Partial<BenutzerhinweisResponseDTO> = {
+  viewId: routeName.value,
+  funktionsbeschreibung: "",
+  bedienung: "",
+  pruefungVorgaben: "",
+};
+
+const benutzerhinweiseDomain = t("model.benutzerhinweis.modelName");
+async function handleSave(
+  benutzerhinweis: Partial<BenutzerhinweisResponseDTO>
+) {
+  if (displayMode.value === InputDisplayMode.CREATE) {
+    await createBenutzerhinweis({
+      benutzerhinweisCreateDTO: benutzerhinweis as BenutzerhinweisCreateDTO,
+    });
+  } else if (displayMode.value === InputDisplayMode.EDIT) {
+    await updateBenutzerhinweis({
+      benutzerhinweisUpdateDTO: benutzerhinweis as BenutzerhinweisUpdateDTO,
+      id: routeName.value,
+    });
+  }
+  if (!createBenutzerhinweisError.value && !updateBenutzerhinweisError.value) {
+    await onSuccess(t("common.message.updated", [benutzerhinweiseDomain]));
+  } else {
+    onFailure(t("common.message.updatedError", [benutzerhinweiseDomain]));
+  }
+}
+
+const snackbarStore = useSnackbarStore();
+const onSuccess = async (msg: string) => {
+  snackbarStore.push({ text: msg, color: STATUS_INDICATORS.SUCCESS });
+  await getBenutzerhinweis({
+    id: routeName.value,
+  });
+  showBenutzerhinweisDialog.value = false;
+};
+const onFailure = (msg: string) => {
+  snackbarStore.push({ text: msg, color: STATUS_INDICATORS.ERROR });
+};
 </script>
