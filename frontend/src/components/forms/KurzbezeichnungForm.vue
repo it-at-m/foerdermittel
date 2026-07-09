3f9809ea282be990c1fa31b640eee1f4ac9fe318
xGnouHQ <181658429+xGnouHQ@users.noreply.github.com>
2026-07-09 16:41:48 +0200

    :construction: WIP Kurzbezeichnung frontend route and component

---
+++ frontend/src/components/forms/KurzbezeichnungForm.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-form
+    ref="form"
+    :readonly="displayMode === InputDisplayMode.READ"
+    @update:model-value="onValidityChanged"
+  >
+    <v-row>
+      <v-col cols="3">
+        <fm-text-field
+          v-model="modelValue.kurzbez"
+          :display-mode="displayMode"
+          disable-edit
+          required
+          uppercase
+          :counter="3"
+          :rules="[
+            rules.required(),
+            rules.minLength(1),
+            rules.maxLength(3),
+            rules.pattern(/^[A-Z]{1,3}$/),
+            rules['unique']!(
+              kurzbezeichnungFormContext.kurzBez,
+              currentKurzBez
+            ),
+          ]"
+          :label="t('model.kurzbezeichnung.kurzbez')"
+        />
+      </v-col>
+      <v-col cols="9">
+        <fm-text-field
+          v-model="modelValue.bezeichnung"
+          :display-mode="displayMode"
+          required
+          :counter="200"
+          :rules="[rules.required(), rules.maxLength(200)]"
+          :label="t('model.kurzbezeichnung.bezeichnung')"
+        />
+      </v-col>
+    </v-row>
+  </v-form>
+</template>
+
+<script setup lang="ts">
+import type {
+  KurzbezeichnungFormContext,
+  KurzbezeichnungResponseDTO,
+} from "@/api/generated/foerdermittel-backend";
+import type { DeepReadonly } from "vue";
+import type { VForm } from "vuetify/components";
+
+import { ref, useTemplateRef } from "vue";
+import { useI18n } from "vue-i18n";
+import { useRules } from "vuetify/labs/rules";
+
+import FmTextField from "@/components/common/FmTextField.vue";
+import { InputDisplayMode } from "@/types/InputDisplayMode";
+
+const { t } = useI18n();
+
+const modelValue = defineModel<Partial<KurzbezeichnungResponseDTO>>({
+  required: true,
+});
+
+// Reactivity is intentionally dropped here to maintain the initial state when form gets mounted.
+const currentKurzBez = ref(modelValue.value.kurzbez);
+
+const { kurzbezeichnungFormContext, displayMode = InputDisplayMode.CREATE } =
+  defineProps<{
+    kurzbezeichnungFormContext: DeepReadonly<KurzbezeichnungFormContext>;
+    displayMode?: InputDisplayMode;
+  }>();
+
+const emit = defineEmits<{
+  isValid: [boolean | null];
+}>();
+
+function onValidityChanged(newIsValid: boolean | null) {
+  emit("isValid", newIsValid);
+}
+
+const rules = useRules();
+
+const formRef = useTemplateRef<VForm>("form");
+async function validate() {
+  if (formRef.value) {
+    await formRef.value.validate();
+  }
+}
+defineExpose({
+  validate,
+});
+</script>
---
+++ frontend/src/routes/kurzbezeichnungen.vue
@@ -0,0 +1,167 @@
+<template>
+  <base-view :domain-key="domainKey">
+    <template #default="{ baseViewLoading }">
+      <!-- @vue-generic {Partial<KurzbezeichnungResponseDTO>} -->
+      <crud-card
+        ref="crudRef"
+        v-model="dataTableOptions"
+        :empty-item-template="EMPTY_ITEM_TEMPLATE"
+        :loading="loading || baseViewLoading"
+        :table-headers="headers"
+        :domain-key="domainKey"
+        :enable-actions="isAdmin"
+        :items="kurzbezeichnungen?.content ?? []"
+        :total-items="kurzbezeichnungen?.page?.totalElements ?? 0"
+        @delete="handleDelete"
+        @create="handleCreate"
+        @update="handleUpdate"
+      >
+        <template #form="{ item, updateValidity, inputDisplayMode }">
+          <kurzbezeichnung-form
+            v-if="kurzbezeichnungFormContext"
+            ref="kurzbezeichnungForm"
+            :model-value="item"
+            :display-mode="inputDisplayMode"
+            :kurzbezeichnung-form-context="kurzbezeichnungFormContext"
+            @is-valid="updateValidity"
+          />
+        </template>
+      </crud-card>
+    </template>
+  </base-view>
+</template>
+
+<script setup lang="ts">
+import type { KurzbezeichnungResponseDTO } from "@/api/generated/foerdermittel-backend";
+import type { DataTableHeader } from "vuetify/framework";
+
+import { computed, useTemplateRef } from "vue";
+import { useI18n } from "vue-i18n";
+
+import BaseView from "@/components/common/BaseView.vue";
+import CrudCard from "@/components/common/CrudCard.vue";
+import KurzbezeichnungForm from "@/components/forms/KurzbezeichnungForm.vue";
+import {
+  useCreateKurzbezeichnung,
+  useDeleteKurzbezeichnung,
+  useGetKurzbezeichnungen,
+  useGetKurzbezeichnungFormContext,
+  useUpdateKurzbezeichnung,
+} from "@/composables/api/useKurzbezeichnungApi";
+import useHasAnyRole from "@/composables/useHasAnyRole";
+import usePagination from "@/composables/usePagination";
+import { Role } from "@/types/Role";
+
+const domainKey = "model.kurzbezeichnung.modelName";
+
+const isAdmin = useHasAnyRole(Role.ADMIN);
+
+const { t } = useI18n();
+
+const headers: DataTableHeader<Partial<KurzbezeichnungResponseDTO>>[] = [
+  {
+    title: t("model.kurzbezeichnung.kurzbez"),
+    value: "kurzbez",
+    align: "center",
+    width: 150,
+  },
+  { title: t("model.kurzbezeichnung.bezeichnung"), value: "bezeichnung" },
+];
+
+const EMPTY_ITEM_TEMPLATE: Partial<KurzbezeichnungResponseDTO> = {
+  kurzbez: undefined,
+  bezeichnung: "",
+};
+
+const {
+  data: kurzbezeichnungen,
+  call: getKurzbezeichnungen,
+  loading: getKurzbezeichnungenLoading,
+} = useGetKurzbezeichnungen();
+
+const {
+  data: kurzbezeichnungFormContext,
+  call: getKurzbezeichnungFormContext,
+  loading: getKurzbezeichnungFormContextLoading,
+} = useGetKurzbezeichnungFormContext();
+
+type KurzbezeichnungFormType = InstanceType<typeof KurzbezeichnungForm>;
+const kurzbezeichnungFormRef = useTemplateRef<KurzbezeichnungFormType>(
+  "kurzbezeichnungForm");
+
+const { dataTableOptions, onSuccess, onFailure } = usePagination(
+  computed(() => kurzbezeichnungen.value?.page?.totalPages),
+  getKurzbezeichnungen,
+  getKurzbezeichnungFormContext,
+  () => kurzbezeichnungFormRef.value?.validate()
+);
+
+const {
+  call: createKurzbezeichnung,
+  loading: createKurzbezeichnungLoading,
+  error: createKurzbezeichnungenError,
+} = useCreateKurzbezeichnung();
+
+const handleCreate = async (
+  kurzbezeichnungCreateDTO: Partial<KurzbezeichnungResponseDTO>
+) => {
+  // TODO: some type checking improvements
+  const model = kurzbezeichnungCreateDTO as KurzbezeichnungResponseDTO;
+  await createKurzbezeichnung({
+    kurzbezeichnungCreateDTO: model,
+  });
+  if (!createKurzbezeichnungenError.value) {
+    await onSuccess(t("common.message.created", [t(domainKey)]));
+  } else {
+    await onFailure(t("common.message.createdError", [t(domainKey)]));
+  }
+};
+
+const {
+  call: updateKurzbezeichnung,
+  loading: updateKurzbezeichnungLoading,
+  error: updateKurzbezeichnungenError,
+} = useUpdateKurzbezeichnung();
+
+const handleUpdate = async (
+  kurzbezeichnungUpdateDTO: Partial<KurzbezeichnungResponseDTO>
+) => {
+  // TODO: some type checking improvements
+  const model = kurzbezeichnungUpdateDTO as KurzbezeichnungResponseDTO;
+  await updateKurzbezeichnung({
+    id: model.id,
+    kurzbezeichnungUpdateDTO: model,
+  });
+  if (!updateKurzbezeichnungenError.value) {
+    await onSuccess(t("common.message.updated", [t(domainKey)]));
+  } else {
+    await onFailure(t("common.message.updatedError", [t(domainKey)]));
+  }
+};
+
+const {
+  call: deleteKurzbezeichnung,
+  loading: deleteKurzbezeichnungLoading,
+  error: deleteKurzbezeichnungenError,
+} = useDeleteKurzbezeichnung();
+
+const handleDelete = async (id: string) => {
+  await deleteKurzbezeichnung({
+    id,
+  });
+  if (!deleteKurzbezeichnungenError.value) {
+    await onSuccess(t("common.message.deleted", [t(domainKey)]));
+  } else {
+    await onFailure(t("common.message.deletedError", [t(domainKey)]));
+  }
+};
+
+const loading = computed(
+  () =>
+    getKurzbezeichnungenLoading.value ||
+    getKurzbezeichnungFormContextLoading.value ||
+    createKurzbezeichnungLoading.value ||
+    updateKurzbezeichnungLoading.value ||
+    deleteKurzbezeichnungLoading.value
+);
+</script>
