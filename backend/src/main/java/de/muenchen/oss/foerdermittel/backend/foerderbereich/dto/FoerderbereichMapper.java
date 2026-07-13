01db6beb96b1a92e32f3f8c9b8a6b423d82cee08
maclhm <267319192+maclhm@users.noreply.github.com>
2026-07-13 14:26:22 +0200

    CR improvements

--- backend/src/main/java/de/muenchen/oss/foerdermittel/backend/foerderbereich/dto/FoerderbereichMapper.java
+++ backend/src/main/java/de/muenchen/oss/foerdermittel/backend/foerderbereich/dto/FoerderbereichMapper.java
@@ -6,7 +6,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(uses = NumberMapper.class)
-@SuppressWarnings("PMD.AvoidDuplicateLiterals")
+
 public interface FoerderbereichMapper {
 
     @Mapping(source = "fb", target = "id", qualifiedByName = "bigDecimalToIntegerString")
