344be1cbc56162a7f7634b6d466ec8e2a3eb6f4e
devtobi <28538704+devtobi@users.noreply.github.com>
2026-07-30 17:18:30 +0200

    :label: Added OpenAPIValidationAttributes to hold mappable information from generated ValidationAttributeMaps

---
+++ frontend/src/types/OpenAPIValidationAttributes.ts
@@ -0,0 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import type { RuleAliases } from "vuetify/labs/rules";
+
+/**
+ * This interface holds most available in generated *ValidationAttributesMap types via the OpenAPI generator.
+ * Only the attributes that are mappable to a corresponding Vuetify {@link RuleAliases} are included.
+ * See
+ */
+export interface ValidationAttributes {
+  dataType: string;
+  required?: boolean;
+  maxLength?: number;
+  minLength?: number;
+  pattern?: string;
+  maximum?: number;
+  exclusiveMaximum?: boolean;
+  minimum?: number;
+  exclusiveMinimum?: boolean;
+}
