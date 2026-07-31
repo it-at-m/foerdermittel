78165a7d7902c0e81784b674c72c0d1f52c59972
devtobi <28538704+devtobi@users.noreply.github.com>
2026-07-31 17:00:28 +0200

    :recycle: Implemented reusable composable for OpenAPI input validation related code

---
+++ frontend/src/composables/useInputValidation.ts
@@ -0,0 +1,70 @@
+import type { ValidationAttributes } from "@/types/OpenAPIValidationAttributes";
+import type { ValidationRule } from "vuetify/framework";
+
+import { computed } from "vue";
+import { useRules } from "vuetify/labs/rules";
+
+import { InputDisplayMode } from "@/types/InputDisplayMode";
+import {
+  getOpenAPIValidationConstraint,
+  mapOpenAPIToVuetifyValidationRules,
+} from "@/util/validation";
+
+export function useOpenApiInput(
+  validationAttributeMap: Record<string, ValidationAttributes>,
+  validationAttributeKey: string,
+  additionalRules: ValidationRule[],
+  displayMode: InputDisplayMode,
+  disableEdit: boolean
+) {
+  const required = computed(
+    () =>
+      (validationAttributeMap &&
+        validationAttributeKey &&
+        getOpenAPIValidationConstraint(
+          validationAttributeMap,
+          validationAttributeKey,
+          "required"
+        )) ??
+      false
+  );
+
+  const rules = useRules();
+  const allRules = computed(() => {
+    if (!validationAttributeMap || !validationAttributeKey) {
+      return additionalRules;
+    }
+
+    return [
+      ...mapOpenAPIToVuetifyValidationRules(
+        rules,
+        validationAttributeMap,
+        validationAttributeKey
+      ),
+      ...additionalRules,
+    ];
+  });
+
+  const counter = computed(() =>
+    !validationAttributeMap || !validationAttributeKey
+      ? undefined
+      : (getOpenAPIValidationConstraint(
+          validationAttributeMap,
+          validationAttributeKey,
+          "maxLength"
+        ) as number)
+  );
+
+  const canNotEdit = computed(
+    () =>
+      displayMode === InputDisplayMode.READ ||
+      (displayMode === InputDisplayMode.EDIT && disableEdit)
+  );
+
+  return {
+    required,
+    allRules,
+    counter,
+    canNotEdit,
+  };
+}
