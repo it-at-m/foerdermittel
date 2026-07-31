ab7443cd02551ca35cbf926f8d2ff251367a5a44
devtobi <28538704+devtobi@users.noreply.github.com>
2026-07-31 10:46:18 +0200

    :recycle: Refactored FmNumberInput to use ValidationAttributesMap

--- frontend/src/components/common/FmNumberInput.vue
+++ frontend/src/components/common/FmNumberInput.vue
@@ -10,6 +10,8 @@
       'pointer-events-none':
         displayMode === InputDisplayMode.READ || canNotEdit,
     }"
+    :counter="counter"
+    :rules="allRules"
     v-bind="$attrs"
   >
     <template #label>
@@ -24,22 +26,76 @@
   </v-number-input>
 </template>
 
-<script setup lang="ts">
+<script
+  setup
+  lang="ts"
+  generic="M extends Record<string, ValidationAttributes>, K extends keyof M"
+>
+import type { ValidationAttributes } from "@/types/OpenAPIValidationAttributes";
+import type { ValidationRule } from "vuetify/framework";
+
 import { computed } from "vue";
 import { useI18n } from "vue-i18n";
+import { useRules } from "vuetify/labs/rules";
 
 import { InputDisplayMode } from "@/types/InputDisplayMode";
+import {
+  getOpenAPIValidationConstraint,
+  mapOpenAPIToVuetifyValidationRules,
+} from "@/util/validation";
 
 const {
   displayMode = InputDisplayMode.CREATE,
   disableEdit = false,
-  required = false,
+  validationAttributeMap,
+  validationAttributeKey,
+  additionalRules = [],
 } = defineProps<{
   label: string;
   displayMode?: InputDisplayMode;
   disableEdit?: boolean;
-  required?: boolean;
+  validationAttributeMap?: M;
+  validationAttributeKey?: K;
+  additionalRules?: ValidationRule[];
 }>();
+
+const required = computed(
+  () =>
+    (validationAttributeMap &&
+      validationAttributeKey &&
+      getOpenAPIValidationConstraint(
+        validationAttributeMap,
+        validationAttributeKey,
+        "required"
+      )) ??
+    false
+);
+
+const rules = useRules();
+const allRules = computed(() => {
+  if (!validationAttributeMap || !validationAttributeKey) {
+    return additionalRules;
+  }
+
+  return [
+    ...mapOpenAPIToVuetifyValidationRules(
+      rules,
+      validationAttributeMap,
+      validationAttributeKey
+    ),
+    ...additionalRules,
+  ];
+});
+
+const counter = computed(() =>
+  !validationAttributeMap || !validationAttributeKey
+    ? undefined
+    : getOpenAPIValidationConstraint(
+        validationAttributeMap,
+        validationAttributeKey,
+        "maxLength"
+      )
+);
 
 const canNotEdit = computed(
   () => displayMode === InputDisplayMode.EDIT && disableEdit
