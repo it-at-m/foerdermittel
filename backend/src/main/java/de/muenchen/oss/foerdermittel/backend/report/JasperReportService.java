f47ca9b13b7553f8b340dfd568afd2ff079f8ec1
devtobi <28538704+devtobi@users.noreply.github.com>
2026-08-10 17:11:49 +0200

    :lock: Refactor JasperReportService to use typed reports only

--- backend/src/main/java/de/muenchen/oss/foerdermittel/backend/report/JasperReportService.java
+++ backend/src/main/java/de/muenchen/oss/foerdermittel/backend/report/JasperReportService.java
@@ -2,12 +2,12 @@ package de.muenchen.oss.foerdermittel.backend.report;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
 import javax.sql.DataSource;
+
 import lombok.RequiredArgsConstructor;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperExportManager;
@@ -20,25 +20,17 @@ import net.sf.jasperreports.export.SimpleExporterInput;
 import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
 import net.sf.jasperreports.export.SimpleXlsxReportConfiguration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class JasperReportService {
 
-    private static final String XLSX_CONTENT_TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
-
-    public enum ReportFormat {
-        PDF,
-        XLSX
-    }
-
     private final DataSource dataSource;
 
-    public byte[] generateReportWithParameters(final String reportName, final Map<String, Object> parameters, final ReportFormat format)
+    public byte[] generateReportWithParameters(final ReportType reportType, final Map<String, Object> parameters, final ReportFormat format)
             throws IOException, SQLException, JRException {
-        final JasperReport jasperReport = loadCompiledReport(reportName);
+        final JasperReport jasperReport = loadCompiledReport(reportType);
 
         final JasperPrint jasperPrint = JasperFillManager.fillReport(
                 jasperReport,
@@ -48,32 +40,22 @@ public class JasperReportService {
         return exportReport(jasperPrint, format);
     }
 
-    public static MediaType getContentType(final ReportFormat format) {
-        return switch (format) {
-        case PDF -> MediaType.APPLICATION_PDF;
-        case XLSX -> MediaType.valueOf(XLSX_CONTENT_TYPE);
-        };
-    }
-
-    public static String getFilename(final String reportName, final ReportFormat format) {
+    public static String getFilename(final ReportType reportType, final ReportFormat reportFormat) {
         final String timestamp = LocalDateTime.now()
                 .format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
-        final String fileExtension = format.toString();
-        return String.format("%s_%s.%s", reportName, timestamp, fileExtension);
+        return String.format("%s_%s.%s", reportType.getFileName(), timestamp, reportFormat.getFileExtension());
     }
 
-    private static JasperReport loadCompiledReport(final String reportName) throws IOException, JRException {
-        final ClassPathResource resource = new ClassPathResource("reports/" + reportName + ".jasper");
+    private static JasperReport loadCompiledReport(final ReportType reportType) throws IOException, JRException {
+        final ClassPathResource resource = new ClassPathResource(reportType.getResourcePath());
 
-        try (InputStream inputStream = resource.getInputStream()) {
-            return (JasperReport) JRLoader.loadObject(inputStream);
-        }
+        return (JasperReport) JRLoader.loadObject(resource.getURL());
     }
 
     private static byte[] exportReport(final JasperPrint jasperPrint, final ReportFormat format) throws JRException {
         return switch (format) {
         case PDF -> JasperExportManager.exportReportToPdf(jasperPrint); // TODO CHECK IF CUSTOM LAYOUT IS NEEDED
-        case XLSX -> JasperReportService.exportReportToXlsx(jasperPrint);
+        case EXCEL -> exportReportToXlsx(jasperPrint);
         };
     }
 
