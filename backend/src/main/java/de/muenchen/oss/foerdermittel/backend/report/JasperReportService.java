fc11d4b3b7180bfeb5ad8513fc6a90efdcbe43f7
devtobi <28538704+devtobi@users.noreply.github.com>
2026-08-11 17:02:55 +0200

    :memo: Added JavaDoc

--- backend/src/main/java/de/muenchen/oss/foerdermittel/backend/report/JasperReportService.java
+++ backend/src/main/java/de/muenchen/oss/foerdermittel/backend/report/JasperReportService.java
@@ -33,7 +33,20 @@ public class JasperReportService {
 
     private final DataSource dataSource;
 
-    public byte[] generateReportWithParameters(final ReportType reportType, final Map<String, Object> parameters, final ReportFormat format)
+    /**
+     * Generates a Jasper report file as byte array given a specific report type, parameters and the
+     * desired report format.
+     *
+     * @param reportType requested report type
+     * @param parameters user provided parameters
+     * @param reportFormat requested report format
+     * @return byte array representing the generated file
+     * @throws IOException when access to the requested report located in the classpath was not possible
+     * @throws SQLException when an error with the database connection occurs
+     * @throws JRException when JasperReport related error (e.g. loading, filling or exporting the
+     *             report) occur
+     */
+    public byte[] generateReportWithParameters(final ReportType reportType, final Map<String, Object> parameters, final ReportFormat reportFormat)
             throws IOException, SQLException, JRException {
         final JasperReport jasperReport = loadCompiledReport(reportType);
 
@@ -48,12 +61,45 @@ public class JasperReportService {
         }
     }
 
+    /**
+     * Generates a Jasper report file as byte array given a specific report type and the desired report
+     * format.
+     *
+     * @param reportType requested report type
+     * @param reportFormat requested report format
+     * @return byte array representing the generated file
+     * @throws IOException when access to the requested report located in the classpath was not possible
+     * @throws SQLException when an error with the database connection occurs
+     * @throws JRException when JasperReport related error (e.g. loading, filling or exporting the
+     *             report) occur
+     */
+    public byte[] generateReport(final ReportType reportType, final ReportFormat reportFormat)
+            throws IOException, SQLException, JRException {
+        return generateReportWithParameters(reportType, Collections.emptyMap(), reportFormat);
+    }
+
+    /**
+     * Calculates the file name depending on the desires report type and report format and uses
+     * timestamps as prefixes
+     *
+     * @param reportType requested report type
+     * @param reportFormat requested report format
+     * @return file name of the file to be generated
+     */
     public static String getFilename(final ReportType reportType, final ReportFormat reportFormat) {
         final String timestamp = LocalDateTime.now()
                 .format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
         return String.format("%s_%s.%s", reportType.getFileName(), timestamp, reportFormat.getFileExtension());
     }
 
+    /**
+     * Loads a compiled `.jasper` file from class path using a given ReportType.
+     *
+     * @param reportType type of report to load
+     * @return JasperReport object
+     * @throws IOException when error in file access occurs
+     * @throws JRException when error loading the file content as a JasperReport occurs
+     */
     private static JasperReport loadCompiledReport(final ReportType reportType) throws IOException, JRException {
         final ClassPathResource resource = new ClassPathResource(reportType.getResourcePath());
 
@@ -102,6 +148,13 @@ public class JasperReportService {
         };
     }
 
+    /**
+     * Exports a filled JasperPrint as a byte array reprenting an `.xlsx` file.
+     *
+     * @param jasperPrint the filled jasper print object
+     * @return byte array representing the `.xlsx` file
+     * @throws JRException when error in `.xlsx` creation occurs
+     */
     private static byte[] exportReportToXlsx(final JasperPrint jasperPrint) throws JRException {
         final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
