2384175e67bb5310c416b81413abd34aead88ad0
devtobi <28538704+devtobi@users.noreply.github.com>
2026-08-14 12:33:19 +0200

    :sparkles: Improved index page to show text and optional no role message

--- frontend/src/routes/index.vue
+++ frontend/src/routes/index.vue
@@ -1,3 +1,40 @@
 <template>
-  <v-container class="fill-height" />
+  <v-container
+    class="fill-height d-flex flex-column align-center justify-center"
+  >
+    <div class="text-center">
+      <h1 class="text-headline-medium mt-0">
+        {{ t("view.index.welcomeTitle") }}
+      </h1>
+      <p
+        v-if="hasRole"
+        class="text-body-large"
+      >
+        {{ t("view.index.welcomeText") }}
+      </p>
+      <v-alert
+        v-else
+        :text="t('common.message.noPermission')"
+        :icon="mdiShieldLock"
+        type="error"
+        class="flex-grow-0"
+      />
+    </div>
+  </v-container>
 </template>
+
+<script setup lang="ts">
+import { mdiShieldLock } from "@mdi/js";
+import { useI18n } from "vue-i18n";
+
+import useHasAnyRole from "@/composables/useHasAnyRole";
+import { Role } from "@/types/Role";
+
+const { t } = useI18n();
+
+const hasRole = useHasAnyRole([
+  Role.SACHBEARBEITUNG,
+  Role.SACHBEARBEITUNG_HAUSHALT,
+  Role.ADMIN,
+]);
+</script>
