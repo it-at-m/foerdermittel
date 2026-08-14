b59ff220bca3b17611139971eed9d46db3a94e16
devtobi <28538704+devtobi@users.noreply.github.com>
2026-08-14 11:28:26 +0200

    :lock: Added route guards to routes to disallow navigation when ro role available

--- frontend/src/routes/publikationen.vue
+++ frontend/src/routes/publikationen.vue
@@ -56,6 +56,16 @@ const domainKey = "model.publikation.modelName";
 
 const isAdmin = useHasAnyRole(Role.ADMIN);
 
+definePage({
+  meta: {
+    hasAnyRole: [
+      Role.SACHBEARBEITUNG,
+      Role.SACHBEARBEITUNG_HAUSHALT,
+      Role.ADMIN,
+    ],
+  },
+});
+
 const { t } = useI18n();
 
 const headers: DataTableHeader<Partial<PublikationResponseDTO>>[] = [
