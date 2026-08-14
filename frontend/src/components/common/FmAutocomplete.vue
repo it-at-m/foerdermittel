87f4fcaee64a2a8ae53266ab63feae58bc0709d7
maclhm <267319192+maclhm@users.noreply.github.com>
2026-08-14 12:29:49 +0200

    prettier

--- frontend/src/components/common/FmAutocomplete.vue
+++ frontend/src/components/common/FmAutocomplete.vue
@@ -1,38 +1,38 @@
 <template>
   <v-autocomplete
-      v-if="displayMode !== InputDisplayMode.READ"
-      v-model="model"
-      :readonly="canNotEdit"
-      :rules="allRules"
-      v-bind="$attrs"
+    v-if="displayMode !== InputDisplayMode.READ"
+    v-model="model"
+    :readonly="canNotEdit"
+    :rules="allRules"
+    v-bind="$attrs"
   >
     <template #label>
       {{ label }}
       <span
-          v-if="required && !canNotEdit"
-          class="text-red"
-      >{{ t("common.word.required") }}</span
+        v-if="required && !canNotEdit"
+        class="text-red"
+        >{{ t("common.word.required") }}</span
       >
       <span v-if="displayMode === InputDisplayMode.EDIT && canNotEdit">{{
-          t("common.word.readOnly")
-        }}</span>
+        t("common.word.readOnly")
+      }}</span>
     </template>
   </v-autocomplete>
   <v-textarea
-      v-else
-      :model-value="model"
-      :label="label"
-      readonly
-      hide-details
-      variant="plain"
-      v-bind="$attrs"
+    v-else
+    :model-value="model"
+    :label="label"
+    readonly
+    hide-details
+    variant="plain"
+    v-bind="$attrs"
   />
 </template>
 
 <script
-    setup
-    lang="ts"
-    generic="
+  setup
+  lang="ts"
+  generic="
     T,
     M extends Record<string, ValidationAttributes>,
     K extends keyof M & string
@@ -62,15 +62,14 @@ const {
 }>();
 
 const { required, allRules, canNotEdit } = useInputValidation(
-    displayMode,
-    disableEdit,
-    additionalRules,
-    validationAttributeMap,
-    validationAttributeKey
+  displayMode,
+  disableEdit,
+  additionalRules,
+  validationAttributeMap,
+  validationAttributeKey
 );
 
 const model = defineModel<T>();
-
 
 const { t } = useI18n();
 </script>
--- frontend/src/components/forms/StadtbezirkslisteForm.vue
+++ frontend/src/components/forms/StadtbezirkslisteForm.vue
@@ -148,10 +148,10 @@ import { computed, ref, useTemplateRef } from "vue";
 import { useI18n } from "vue-i18n";
 import { useRules } from "vuetify/labs/rules";
 
+import FmAutocomplete from "@/components/common/FmAutocomplete.vue";
 import FmTextField from "@/components/common/FmTextField.vue";
 import { useGetStadtbezirke } from "@/composables/api/useStadtbezirkApi";
 import { InputDisplayMode } from "@/types/InputDisplayMode";
-import FmAutocomplete from "@/components/common/FmAutocomplete.vue";
 
 const { t } = useI18n();
 
