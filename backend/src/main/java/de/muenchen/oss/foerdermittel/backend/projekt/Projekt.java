7e0ac43d449da2b41220cbfa46ccb5e2e4163953
xGnouHQ <181658429+xGnouHQ@users.noreply.github.com>
2026-08-20 17:25:24 +0200

    Fix wildcard imports

--- backend/src/main/java/de/muenchen/oss/foerdermittel/backend/projekt/Projekt.java
+++ backend/src/main/java/de/muenchen/oss/foerdermittel/backend/projekt/Projekt.java
@@ -2,7 +2,14 @@ package de.muenchen.oss.foerdermittel.backend.projekt;
 
 import de.muenchen.oss.foerdermittel.backend.archiv.Archiv;
 import de.muenchen.oss.foerdermittel.backend.foerderbereich.Foerderbereich;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.FetchType;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.io.Serial;
