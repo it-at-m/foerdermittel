1538013b34ff520cf6eeb425340803d3fb520b06
xGnouHQ <181658429+xGnouHQ@users.noreply.github.com>
2026-08-21 12:12:53 +0200

    run spotless

--- backend/src/main/java/de/muenchen/oss/foerdermittel/backend/projekt/Projekt.java
+++ backend/src/main/java/de/muenchen/oss/foerdermittel/backend/projekt/Projekt.java
@@ -1,6 +1,5 @@
 package de.muenchen.oss.foerdermittel.backend.projekt;
 
-import de.muenchen.oss.foerdermittel.backend.archiv.Archiv;
 import de.muenchen.oss.foerdermittel.backend.foerderbereich.Foerderbereich;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -8,13 +7,11 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.io.Serial;
 import java.io.Serializable;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
