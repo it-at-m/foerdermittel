9bfe6cca2c20950c4a2567f8def7989905675221
maclhm <267319192+maclhm@users.noreply.github.com>
2026-08-25 14:50:35 +0200

    changed LocalDate -> OffsetDateTime improvements

--- backend/src/main/java/de/muenchen/oss/foerdermittel/backend/archiv/dto/ArchivMapper.java
+++ backend/src/main/java/de/muenchen/oss/foerdermittel/backend/archiv/dto/ArchivMapper.java
@@ -8,62 +8,68 @@ import org.mapstruct.Mapping;
 @Mapper(uses = TimeZoneMapper.class)
 public interface ArchivMapper {
 
+    String SPEICHER_DATUM = "speicherDatum";
+    String MIKRO_DAT_PLAN = "mikroDatPlan";
+    String MIKRO_DAT = "mikroDat";
+    String OFFSET_TO_LOCAL_DATE = "offSetDateTimeToLocalDate";
+    String LOCAL_DATE_TO_OFFSET = "localDateToOffSetDate";
+
     @Mapping(source = "id", target = "id")
     @Mapping(source = "projekt.projnr", target = "projnr")
     @Mapping(source = "projekt.pname", target = "pname")
     @Mapping(source = "projekt.pstrasse", target = "pstrasse")
     @Mapping(source = "projekt.foerderbereich.fb", target = "fob_fb")
     @Mapping(
-            source = "speicherDatum",
-            target = "speicherDatum",
-            qualifiedByName = "localDateToOffSetDate"
+            source = SPEICHER_DATUM,
+            target = SPEICHER_DATUM,
+            qualifiedByName = LOCAL_DATE_TO_OFFSET
     )
     @Mapping(
-            source = "mikroDatPlan",
-            target = "mikroDatPlan",
-            qualifiedByName = "localDateToOffSetDate"
+            source = MIKRO_DAT_PLAN,
+            target = MIKRO_DAT_PLAN,
+            qualifiedByName = LOCAL_DATE_TO_OFFSET
     )
     @Mapping(
-            source = "mikroDat",
-            target = "mikroDat",
-            qualifiedByName = "localDateToOffSetDate"
+            source = MIKRO_DAT,
+            target = MIKRO_DAT,
+            qualifiedByName = LOCAL_DATE_TO_OFFSET
     )
     ArchivResponseDTO toDTO(Archiv archiv);
 
     @Mapping(source = "projnr", target = "projekt.projnr")
     @Mapping(
-            source = "speicherDatum",
-            target = "speicherDatum",
-            qualifiedByName = "offSetDateTimeToLocalDate"
+            source = SPEICHER_DATUM,
+            target = SPEICHER_DATUM,
+            qualifiedByName = OFFSET_TO_LOCAL_DATE
     )
     @Mapping(
-            source = "mikroDatPlan",
-            target = "mikroDatPlan",
-            qualifiedByName = "offSetDateTimeToLocalDate"
+            source = MIKRO_DAT_PLAN,
+            target = MIKRO_DAT_PLAN,
+            qualifiedByName = OFFSET_TO_LOCAL_DATE
     )
     @Mapping(
-            source = "mikroDat",
-            target = "mikroDat",
-            qualifiedByName = "offSetDateTimeToLocalDate"
+            source = MIKRO_DAT,
+            target = MIKRO_DAT,
+            qualifiedByName = OFFSET_TO_LOCAL_DATE
     )
     Archiv toEntity(ArchivCreateDTO archivCreateDTO);
 
     @Mapping(target = "projekt", ignore = true)
     @Mapping(target = "id", ignore = true)
     @Mapping(
-            source = "speicherDatum",
-            target = "speicherDatum",
-            qualifiedByName = "offSetDateTimeToLocalDate"
+            source = SPEICHER_DATUM,
+            target = SPEICHER_DATUM,
+            qualifiedByName = OFFSET_TO_LOCAL_DATE
     )
     @Mapping(
-            source = "mikroDatPlan",
-            target = "mikroDatPlan",
-            qualifiedByName = "offSetDateTimeToLocalDate"
+            source = MIKRO_DAT_PLAN,
+            target = MIKRO_DAT_PLAN,
+            qualifiedByName = OFFSET_TO_LOCAL_DATE
     )
     @Mapping(
-            source = "mikroDat",
-            target = "mikroDat",
-            qualifiedByName = "offSetDateTimeToLocalDate"
+            source = MIKRO_DAT,
+            target = MIKRO_DAT,
+            qualifiedByName = OFFSET_TO_LOCAL_DATE
     )
     Archiv toEntity(ArchivUpdateDTO archivUpdateDTO);
 
