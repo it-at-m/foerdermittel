7a96ee3f41eb0660a60e636f05cde1d53b203f70
devtobi <28538704+devtobi@users.noreply.github.com>
2026-08-27 14:57:28 +0200

    :construction: Added ReportService to construct GeneratedReport objects, handle validation and jasper service interaction

---
+++ backend/src/main/java/de/muenchen/oss/foerdermittel/backend/report/ReportService.java
@@ -0,0 +1,90 @@
+package de.muenchen.oss.foerdermittel.backend.report;
+
+import de.muenchen.oss.foerdermittel.backend.report.dto.ReportMapper;
+import de.muenchen.oss.foerdermittel.backend.report.dto.ReportStichwortbereicheDTO;
+import de.muenchen.oss.foerdermittel.backend.security.Authorities;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ReportService {
+
+    public static final String SORT_PARAMETER = "P_SORT";
+
+    private final JasperReportService jasperReportService;
+    private final ReportMapper reportMapper;
+
+    @PreAuthorize(Authorities.HAS_ANY_ROLE)
+    @Transactional(readOnly = true)
+    public GeneratedReport generateReportStichwortbereiche(
+            final ReportStichwortbereicheDTO parameters) {
+        return generateReport(reportMapper.toJasperParameters(parameters), ReportType.FMW_ABLAGEINDEX, ReportFormat.PDF,
+                "ORDER BY stb_bereich ASC, nr ASC, wort ASC");
+    }
+
+    /**
+     * Utility function to create a {@link GeneratedReport}.
+     * @param jasperParameters parameters to fill the report with
+     * @param reportType type of the report to generate
+     * @param reportFormat format of the report to generate
+     * @param sort optional sort parameter to pass manually instead of in the mapped parameters
+     * @return the generated report with file metadata
+     */
+    private GeneratedReport generateReport(
+            final Map<String, Object> jasperParameters,
+            final ReportType reportType,
+            final ReportFormat reportFormat,
+            final String sort) {
+
+        checkReportFormat(reportType, reportFormat);
+
+        if (sort != null && !jasperParameters.containsKey(SORT_PARAMETER)) {
+            jasperParameters.put(SORT_PARAMETER, sort);
+        }
+
+        return new GeneratedReport(
+                getDownloadFileName(reportType, reportFormat),
+                reportFormat.getContentType(),
+                outputStream -> {
+                    jasperReportService.generateReportWithParameters(
+                            reportType,
+                            reportFormat,
+                            jasperParameters,
+                            outputStream);
+                });
+    }
+
+    /**
+     * Checks if a given reportFormat is valid for a given ReportType.
+     *
+     * @param reportType the ReportType to check against
+     * @param reportFormat the ReportFormat to check
+     */
+    private static void checkReportFormat(final ReportType reportType, final ReportFormat reportFormat) {
+        if (!reportType.supportsFormat(reportFormat)) {
+            throw new IllegalArgumentException(
+                    "Unsupported ReportFormat: " + reportFormat + " for ReportType: " + reportType);
+        }
+    }
+
+    /**
+     * Calculates the file name depending on the desires report type and report format and uses
+     * timestamps as prefixes
+     *
+     * @param reportType requested report type
+     * @param reportFormat requested report format
+     * @return file name of the file to be generated
+     */
+    private static String getDownloadFileName(final ReportType reportType, final ReportFormat reportFormat) {
+        final String timestamp = LocalDateTime.now()
+                .format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
+        return reportType.getFileName() + reportFormat.getFileSuffix() + "_" + timestamp + reportFormat.getFileExtension();
+    }
+
+}
---
+++ backend/src/test/java/de/muenchen/oss/foerdermittel/backend/report/ReportServiceTest.java
@@ -0,0 +1,97 @@
+package de.muenchen.oss.foerdermittel.backend.report;
+
+import static de.muenchen.oss.foerdermittel.backend.report.ReportService.SORT_PARAMETER;
+import static org.assertj.core.api.Assertions.assertThat;
+import de.muenchen.oss.foerdermittel.backend.report.dto.ReportMapper;
+import de.muenchen.oss.foerdermittel.backend.report.dto.ReportStichwortbereicheDTO;
+import net.sf.jasperreports.engine.JRException;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ReportServiceTest {
+
+    @Mock
+    private JasperReportService jasperReportService;
+
+    @Mock
+    private ReportMapper reportMapper;
+
+    @InjectMocks
+    private ReportService reportService;
+
+    @Nested
+    class GenerateReportStichwortbereiche {
+
+        @Test
+        void givenNoWriteInteraction_thenShouldGenerateCorrectGeneratedReport() {
+            // Given
+            final ReportStichwortbereicheDTO parameters = mock(ReportStichwortbereicheDTO.class);
+
+            final Map<String, Object> jasperParameters = new HashMap<>();
+            when(reportMapper.toJasperParameters(parameters))
+                    .thenReturn(jasperParameters);
+
+            // When
+            final GeneratedReport generatedReport = reportService.generateReportStichwortbereiche(parameters);
+
+            // Then
+            verify(reportMapper, times(1)).toJasperParameters(parameters);
+            verifyNoInteractions(jasperReportService);
+
+            assertThat(generatedReport).isNotNull();
+            assertThat(generatedReport.contentType())
+                    .isEqualTo(ReportFormat.PDF.getContentType());
+            assertThat(generatedReport.fileName())
+                    .startsWith(ReportType.FMW_ABLAGEINDEX.getFileName())
+                    .endsWith(ReportFormat.PDF.getFileExtension());
+            assertThat(jasperParameters)
+                    .containsEntry(
+                            SORT_PARAMETER,
+                            "ORDER BY stb_bereich ASC, nr ASC, wort ASC");
+        }
+
+        @Test
+        void givenWriteInteraction_thenShouldCallJasperServiceCorrectly() throws JRException, SQLException, IOException {
+            // Given
+            final ReportStichwortbereicheDTO parameters = mock(ReportStichwortbereicheDTO.class);
+
+            final Map<String, Object> jasperParameters = new HashMap<>();
+            when(reportMapper.toJasperParameters(parameters))
+                    .thenReturn(jasperParameters);
+
+            final OutputStream outputStream = new ByteArrayOutputStream();
+
+            // When
+            final GeneratedReport generatedReport = reportService.generateReportStichwortbereiche(parameters);
+            generatedReport.writer().write(outputStream);
+
+            // Then
+            verify(jasperReportService, times(1)).generateReportWithParameters(
+                    ReportType.FMW_ABLAGEINDEX,
+                    ReportFormat.PDF,
+                    jasperParameters,
+                    outputStream);
+        }
+
+    }
+
+
+}
