8af976da59402e7179c6496ee719521025334ed5
devtobi <28538704+devtobi@users.noreply.github.com>
2026-08-31 15:55:37 +0200

    :bug: Fix nested routes routeName construction for Benutzerhinweise

--- frontend/src/components/common/BaseView.vue
+++ frontend/src/components/common/BaseView.vue
@@ -76,7 +76,7 @@ const { t } = useI18n();
 
 const route = useRoute();
 // always contains a valid route name, implemented fallback only to make vue-tsc happy without building to generate route-map.d.ts
-const routeName = computed(() => (route.name as string).replace("/", ""));
+const routeName = computed(() => (route.name as string).replaceAll("/", ""));
 
 const emptyItemTemplate = computed<Partial<BenutzerhinweisResponseDTO>>(() => {
   return {
