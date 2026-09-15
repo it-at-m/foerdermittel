071e5a0dc3118dbc1eb623501e4441597194d8d1
devtobi <28538704+devtobi@users.noreply.github.com>
2026-09-15 14:06:20 +0200

    :recycle: Refactored useOpenApiRules and useInputValidation

--- frontend/src/composables/useOpenApiRules.ts
+++ frontend/src/composables/useOpenApiRules.ts
@@ -1,5 +1,6 @@
 import type { ValidationAttributes } from "@/util/validation";
-import type { ComputedRef, MaybeRefOrGetter } from "vue";
+import type { MaybeRefOrGetter } from "vue";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import type { ValidationRule } from "vuetify";
 
 import { computed, toValue } from "vue";
@@ -7,53 +8,61 @@ import { useRules } from "vuetify";
 
 import { mapOpenAPIToVuetifyValidationRules } from "@/util/validation";
 
-export default function useOpenApiRules<
-  T extends Record<string, ValidationAttributes>,
-  K extends keyof T,
->(
-  validationAttributesMap: MaybeRefOrGetter<T | undefined>,
-  property: MaybeRefOrGetter<K | undefined>,
-  trimStringValues?: MaybeRefOrGetter<boolean>
-): ComputedRef<ValidationRule[]>;
-
-export default function useOpenApiRules<
-  T extends Record<string, ValidationAttributes>,
->(
-  validationAttributesMap: MaybeRefOrGetter<T | undefined>,
-  trimStringValues?: MaybeRefOrGetter<boolean>
-): <K extends keyof T>(
-  property: MaybeRefOrGetter<K | undefined>
-) => ComputedRef<ValidationRule[]>;
-
 /**
  * Creates Vuetify {@link ValidationRule}s from OpenAPI-generated validation
  * attributes.
  *
  * See supported rules in {@link VuetifyRuleAliases}.
  *
- * @param validationAttributesMap OpenAPI-generated validation attributes map
- * @param property Optional model property. If omitted, returns a factory function for the whole attributes map.
- * @param trimStringValues Whether string values should be trimmed before validation.
+ * @param options Validation options.
+ * @param options.validationAttributesMap OpenAPI-generated validation attributes map.
+ * @param options.property Optional model property. If omitted, returns a factory
+ * function for the whole attributes map.
+ * @param options.trimStringValues Whether string values should be trimmed before validation.
  */
 export default function useOpenApiRules<
   T extends Record<string, ValidationAttributes>,
   K extends keyof T,
->(
-  validationAttributesMap: MaybeRefOrGetter<T | undefined>,
-  property?: MaybeRefOrGetter<K | undefined>,
-  trimStringValues?: MaybeRefOrGetter<boolean>
-) {
+>({
+  validationAttributesMap,
+  property,
+  trimStringValues,
+}: {
+  validationAttributesMap: MaybeRefOrGetter<T | undefined>;
+  property?: MaybeRefOrGetter<K | undefined>;
+  trimStringValues?: MaybeRefOrGetter<boolean>;
+}) {
   const rules = useRules();
 
-  const calculateRules = (property: MaybeRefOrGetter<K | undefined>) =>
-    computed(() =>
-      mapOpenAPIToVuetifyValidationRules(
-        rules,
-        toValue(validationAttributesMap),
-        toValue(property),
-        toValue(trimStringValues) ?? false
-      )
-    );
+  const valMap = toValue(validationAttributesMap);
 
-  return property === undefined ? calculateRules : calculateRules(property);
+  const calculateRules = <P extends keyof T>(
+    property: MaybeRefOrGetter<P | undefined>
+  ) =>
+    computed(() => {
+      const map = toValue(validationAttributesMap);
+      const key = toValue(property);
+
+      if (map === undefined || key === undefined) {
+        return [];
+      }
+
+      return mapOpenAPIToVuetifyValidationRules(
+        rules,
+        map,
+        key,
+        toValue(trimStringValues) ?? false
+      );
+    });
+
+  if (valMap === undefined) {
+    return computed(() => []);
+  }
+
+  if (property === undefined) {
+    return <P extends keyof T>(property: MaybeRefOrGetter<P | undefined>) =>
+      calculateRules(property);
+  }
+
+  return calculateRules(property);
 }
