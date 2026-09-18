0ae14b4e8fed6e271de17e5f56df0207b2133a99
xGnouHQ <181658429+xGnouHQ@users.noreply.github.com>
2026-09-18 16:10:52 +0200

    Added Enum Krisofp

---
+++ backend/src/main/java/de/muenchen/oss/foerdermittel/backend/projekt/Krisofp.java
@@ -0,0 +1,13 @@
+package de.muenchen.oss.foerdermittel.backend.projekt;
+
+public enum Krisofp {
+
+    X, // 1.+2. SIP - KriSoFö 2008-2014
+    Y, // 3. SIP - Kinderbetreuung 2015-2019
+    Z, // 4. SIP - Kinderbetreuung 2017-2020/2021
+    W, // 5. SIP - Schulkindbetreuung Land (Hortförd. GS)
+    V, // Schulkindbetreuung - Bund (GaFöG)
+    K, // KJP II
+    F, // FAGplus15
+    S  // Startchancen-Programm (SCP)
+}
